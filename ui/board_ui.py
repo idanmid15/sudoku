8b8f1c05bc0cd1c12b5087e5fcd27a00d62b02b9
Idan Midroni <idanmid15@gmail.com>
2019-02-02 23:44:14 +0200

    Solver now solves the given ui board and displays it to the stdout

--- ui/board_ui.py
+++ ui/board_ui.py
@@ -1,10 +1,21 @@
 
 from tkinter import *
-from board import Board
+import tkinter.messagebox
+from board import Board, empty_sign
+from solver import solve_game, recursive_solve_improved
+from movement import guess_cell_valid
 
 
-def print_event(x, y):
-    print("({}, {}), value: {}".format(x, y, board_entries[x][y].get()))
+def entered_value(x, y):
+    given_num = board_entries[x][y].getint()
+    if guess_cell_valid(board, x, y, given_num):
+        board.set_cell(x, y, given_num)
+        solve_game(board, recursive_solve_improved)
+    else:
+        tkinter.messagebox.showinfo("Alert", "The cell value of {} which was entered is illegal!".format(given_num))
+        board.set_cell(x, y, empty_sign)
+        board_entries[x][y].delete(0, END)
+        board_entries[x][y].insert(END, empty_sign)
 
 
 def create_board_entries(board_cells):
@@ -13,17 +24,14 @@ def create_board_entries(board_cells):
         for j, _ in enumerate(board_cells[i]):
             entry = Entry(root, justify=CENTER, bd=3)
             entry.grid(row=i, column=j)
-            entry.insert(END, board[i][j])
-            entry.bind("<Return>", lambda event, a=i, b=j: print_event(a, b))
+            entry.insert(END, board_cells[i][j])
+            entry.bind("<Return>", lambda event, a=i, b=j: entered_value(a, b))
             entries[i][j] = entry
     return entries
 
 
 if __name__ == '__main__':
     root = Tk()
-    board = Board().get_all_cells()
-    board_entries = create_board_entries(board)
-
-
-root.mainloop()
-
+    board = Board()
+    board_entries = create_board_entries(board.get_all_cells())
+    root.mainloop()
